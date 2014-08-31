de3622afb5cfc277960874789790b78ee88e96ec
Brandon Huddle <bandoncontortion@gmail.com>
2014-08-31 17:44:20 -0400

    Clean files
    Not needed

--- Test.cpp
+++
@@ -1,9 +0,0 @@
-#define XLGL_APP_NAME "XLGL Examples"
-#include <XLGL/gl.h>
-#include <XLGL/xlglmm.hpp>
-
-int main(int argc, char* argv[]) {
-    XLGL::Window* window;
-    XLGL::AndroidSetupSavedState(window);
-    return 0;
-}
